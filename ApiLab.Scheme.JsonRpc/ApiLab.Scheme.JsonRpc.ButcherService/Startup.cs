fc652cf1846b73572c7766e6ff9ae288e01791e4
Ron MacNeil <macro@hotmail.com>
2018-04-20 22:24:15 +1000

    JsonRpc: Initial cut of Butcher service

---
+++ ApiLab.Scheme.JsonRpc/ApiLab.Scheme.JsonRpc.ButcherService/Startup.cs
@@ -0,0 +1,27 @@
+﻿using ApiLab.OneTrue.Butcher;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiLab.Scheme.JsonRpc.ButcherService
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddJsonRpc(options => {
+                options.ShowServerExceptions = true;
+            });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseManualJsonRpc(builder => {
+                builder.RegisterController<OneTrueButcher>();
+            });
+        }
+
+    }
+}
